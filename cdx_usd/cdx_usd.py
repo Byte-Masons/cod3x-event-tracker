6ac4e775f1d5fbf77256fc5d53f9e7208c2253f1
Daniel <daniel.dean.mist@gmail.com>
2024-11-21 13:59:34 -0500

    cdx_usd now executes and refreshes query data when called

--- cdx_usd/cdx_usd.py
+++ cdx_usd/cdx_usd.py
@@ -1,14 +1,45 @@
 import requests
 import pandas as pd
 from io import StringIO
+import time
+
+DUNE_KEY = 'cp6G2OF5wnUnpc4xblcBc5nKq43As6UM'
+QUERY_ID = 4289320
+
+# # executes our query to refresh data
+def execute_query():
+
+    url = f"https://api.dune.com/api/v1/query/{QUERY_ID}/execute"
+
+    headers = {"X-DUNE-API-KEY": DUNE_KEY}
+
+    response = requests.request("POST", url, headers=headers)
+
+    execution_id = response.json()['execution_id']
+
+    return execution_id
+
+def get_query_status(execution_id):
+    """
+    Takes in an execution ID.
+    Fetches the status of query execution using the API
+    Returns the status response object
+    """
+    url = f"https://api.dune.com/api/v1/execution/{execution_id}/status"
+
+    headers = {"X-DUNE-API-KEY": DUNE_KEY}
+
+    response = requests.request("GET", url, headers=headers)
+
+    return response.json()['is_execution_finished']
 
 def get_cdx_usd_revenue_df():
     
     df = pd.DataFrame()
 
-    url = 'https://api.dune.com/api/v1/query/4289320/results/csv'
+    url = f'https://api.dune.com/api/v1/query/{QUERY_ID}/results/csv'
     headers = {
-        'X-Dune-API-Key': ''
+        'X-Dune-API-Key': DUNE_KEY
     }
     params = {
         'limit': 25000
@@ -25,7 +56,7 @@ def get_cdx_usd_revenue_df():
         # print(df.head())
     else:
         print(f"Request failed with status code: {response.status_code}")
-    
+
     df['day'] = pd.to_datetime(df['day']).dt.strftime('%Y/%m/%d')
 
     return df
@@ -47,7 +78,18 @@ def format_df(df):
 
     return df
 
+# # executes our query to refresh the data
+# # then returns a nicely formatted dataframe
 def run_all():
+
+    execution_id = execute_query()
+
+    query_is_finished = get_query_status(execution_id)
+
+    # # will wait for our query to finish
+    while query_is_finished == False:
+        time.sleep(2.5)
+        query_is_finished = get_query_status(execution_id)
 
     df = get_cdx_usd_revenue_df()
 
@@ -55,7 +97,5 @@ def run_all():
 
     return df
 
-
-
-# df = get_cdx_usd_revenue_df()
+# df = run_all()
 # print(df)
