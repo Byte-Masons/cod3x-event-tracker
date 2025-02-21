0e2f6dd85e756c43ade27b74f2531edbb5b39146
Daniel <daniel.dean.mist@gmail.com>
2025-02-20 21:38:12 -0500

    fixed calculation bugs

--- cdx_usd/MBTC_Position_Tracker.py
+++ cdx_usd/MBTC_Position_Tracker.py
@@ -54,6 +54,7 @@ def make_unique_address_df(unique_user_list):
 
     return df
 
+# # user data
 def get_user_reserves_data(contract, user_address):
     try:
         output = contract.functions.getUserReservesData(LENDING_POOL_ADDRESS_PROVIDER_ADDRESS, user_address).call()
@@ -86,7 +87,102 @@ def get_user_reserves_data(contract, user_address):
     except Exception as e:
         print(f"Error: {e}")
         return pd.DataFrame()  # Return empty DataFrame on error
+    
+# # token data
+def get_reserves_data(contract, lending_pool_address_provider_address):
+    
+    try:
+        output = contract.functions.getReservesData(lending_pool_address_provider_address).call()
+        output = output[0]
+        # Create a list to store each reserve's data
+        reserves_data = []
+        
+        for reserve in output:
+            print(reserve)
+            data = {
+                'underlyingAsset': reserve[0],
+                'name': reserve[1],
+                'symbol': reserve[2],
+                'decimals': reserve[3],
+                'liquidityIndex': reserve[13],
+                'variableBorrowIndex': reserve[14],
+                'price': reserve[28],
+            }
+            reserves_data.append(data)
+        
+        # Convert to DataFrame
+        df = pd.DataFrame(reserves_data)
+        print(df)
+        
+        # Optional: Convert Wei values to Ether for better readability
+        # wei_columns = ['scaledATokenBalance', 'scaledVariableDebt', 'principalStableDebt']
+        # for col in wei_columns:
+        #     df[f'{col}_ETH'] = df[col] / 1e18
+            
+        return df
+        
+    except Exception as e:
+        print(f"Error: {e}")
+        return pd.DataFrame()  # Return empty DataFrame on error
 
+# # will prune the user_df down to only rows that have a value in either the scaledATokenBalance or principalStableDebt column
+def get_user_non_zero_rows(df):
+    df = df.loc[(df['scaledATokenBalance'] > 0) | (df['scaledVariableDebt'] > 0)]
+
+    return df
+
+# # does our principal and debt calculations after we have our user and token data
+def get_user_principal_and_debt(user_df, token_df):
+
+    token_address_list = token_df['underlyingAsset'].tolist()
+    token_symbol_list = token_df['symbol'].tolist()
+    token_decimals_list = token_df['decimals'].tolist()
+    token_liquidity_index_list = token_df['liquidityIndex'].tolist()
+    token_variable_borrow_index_list = token_df['variableBorrowIndex'].tolist()
+    token_price_list = token_df['price'].tolist()
+
+    i = 0
+
+    while i < len(token_address_list):
+        token_address = token_address_list[i]
+        token_symbol = token_symbol_list[i]
+        token_decimals = token_decimals_list[i]
+        token_liquidity_index = token_liquidity_index_list[i]
+        token_variable_borrow_index = token_variable_borrow_index_list[i]
+        token_price = token_price_list[i]
+
+        user_df.loc[user_df['underlyingAsset'] == token_address, 'symbol'] = token_symbol
+        user_df.loc[user_df['underlyingAsset'] == token_address, 'decimals'] = token_decimals
+        user_df.loc[user_df['underlyingAsset'] == token_address, 'liquidity_index'] = token_liquidity_index
+        user_df.loc[user_df['underlyingAsset'] == token_address, 'variable_liquidity_index'] = token_variable_borrow_index
+        user_df.loc[user_df['underlyingAsset'] == token_address, 'token_price'] = token_price
+
+        i += 1
+
+    # # balances
+    user_df['a_token_balance'] = user_df['scaledATokenBalance'] * user_df['liquidity_index'] / 1e27 / (10 ** user_df['decimals'])
+    user_df['v_token_balance'] = user_df['scaledVariableDebt'] * user_df['variable_liquidity_index'] / 1e27 / (10 ** user_df['decimals'])
+
+    # # usd balances per asset
+    user_df['a_token_balance_usd'] = user_df['a_token_balance'] * (user_df['token_price'] / 1e8)
+    user_df['v_token_balance_usd'] = user_df['v_token_balance'] * (user_df['token_price'] / 1e8)
+    print(user_df)
+
+    # # principal
+    principal_df = user_df.groupby(['user_address'])['a_token_balance_usd'].sum().reset_index()
+    principal_df = principal_df.rename(columns={'a_token_balance_usd': 'total_principal'})  # Rename to avoid column conflict
+    # Merge back to original DataFrame
+    user_df = user_df.merge(principal_df, on='user_address', how='left')
+
+    # # debt
+    debt_df = user_df.groupby(['user_address'])['v_token_balance_usd'].sum().reset_index()
+    debt_df = debt_df.rename(columns={'v_token_balance_usd': 'total_debt'})  # Rename to avoid column conflict
+    # Merge back to original DataFrame
+    user_df = user_df.merge(debt_df, on='user_address', how='left')
+
+    user_df['net_balance'] = user_df['total_principal'] - user_df['total_debt']
+
+    return user_df
 
 def run_all():
     # unique_user_list = get_icl_transactors()
@@ -102,31 +198,48 @@ def run_all():
 
     df_list = []
 
-    i = 1
+    i = 0
 
-    # # finds data for each historical user
-    for unique_user_address in unique_user_address_list:
-        df = get_user_reserves_data(contract, unique_user_address)
+    # # # finds data for each historical user
+    # for unique_user_address in unique_user_address_list:
+    #     df = get_user_reserves_data(contract, unique_user_address)
 
-        if len(df) > 0:
-            df['user_address'] = unique_user_address
+    #     if len(df) > 0:
+    #         df['user_address'] = unique_user_address
 
-            df = df[['user_address', 'underlyingAsset', 'scaledATokenBalance', 'scaledVariableDebt']]
+    #         df = df[['user_address', 'underlyingAsset', 'scaledATokenBalance', 'scaledVariableDebt']]
 
-            df_list.append(df)
-            print(df)
+    #         df_list.append(df)
+    #         print(df)
 
-        else:
-            print('No Data Found: ', unique_user_address)
+    #     else:
+    #         print('No Data Found: ', unique_user_address)
 
-        print('Remaining: ', i, '/', len(unique_user_address_list), 'or ', str( ( len(unique_user_address_list)-i ) *WAIT_TIME), ' seconds left' )
+    #     print('Remaining: ', i, '/', len(unique_user_address_list), 'or ', str( ( len(unique_user_address_list)-i ) *WAIT_TIME), ' seconds left' )
 
-        i += 1
+    #     i += 1
 
-        time.sleep(WAIT_TIME)
+    #     time.sleep(WAIT_TIME)
 
-    df = pd.concat(df_list)
+    # df = pd.concat(df_list)
+    # df.to_csv('icl_user_balance_data.csv', index=False)
 
-    df.to_csv('icl_user_balance_data.csv', index=False)
+    df = pd.read_csv('icl_user_balance_data.csv')
+    df[['scaledATokenBalance', 'scaledVariableDebt']] = df[['scaledATokenBalance', 'scaledVariableDebt']].astype(float)
 
+    token_df = get_reserves_data(contract, LENDING_POOL_ADDRESS_PROVIDER_ADDRESS)
+    token_df.to_csv('icl_token_info_df.csv', index=False)
+    token_df[['decimals', 'liquidityIndex', 'variableBorrowIndex', 'price']] = token_df[['decimals', 'liquidityIndex', 'variableBorrowIndex', 'price']].astype(float)
+
+    df = get_user_non_zero_rows(df)
+
+    print(token_df)
+    print(df)
+    df = get_user_principal_and_debt(df, token_df)
+
+    df = df[['user_address', 'symbol', 'underlyingAsset', 'token_price', 'a_token_balance', 'v_token_balance', 'a_token_balance_usd', 'v_token_balance_usd', 'total_principal', 'total_debt', 'net_balance']]
+    df = df.loc[df['net_balance'] >= 1]
+    df = df.sort_values(by='net_balance', ascending=False)
+    df.to_csv('icl_finalized_user_balances.csv', index=False)
+    
     return df
